193892c011cef1d0a9303d2406dd12f648751fe8
ally haas <allyhaas@gmail.com>
2023-08-03 18:12:18 -0600

    settings updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,8 +47,7 @@ CORS_ALLOWED_ORIGINS = [
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jq)$f!or7y)jkj@!3me#jrc6ohm4=$=7v!1$sa7jla#q4d0(_v"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
